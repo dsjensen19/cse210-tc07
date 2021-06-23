62e09051f9292152d382c8843fb5cb037bec39f9
Seth Burton <sethburton80@gmail.com>
2021-06-23 09:07:53 -0600

    I finished finally sorry for slacking

---
+++ game/word.py
@@ -0,0 +1,21 @@
+from game.point import Point
+from actor import Actor
+from constants import LIBRARY
+import constants
+from random import randint
+from math import(
+    sin,
+    cos,
+    degrees,
+)
+class Word(Actor):
+    def __init__(self):
+        super().__init__()
+        self.text = LIBRARY[randint(0,len(LIBRARY))]
+        x = randint(0,constants.MAX_X)
+        y = randint(0,constants.MAX_Y)
+        self.set_position(Point(x,y))
+        angle = randint(0,359)
+        dx = cos(angle)
+        dy = sin(angle)
+        self.set_velocity(Point(x,dy))
